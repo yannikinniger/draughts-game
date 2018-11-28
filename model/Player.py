9af9ad8dfedf62a309fbb2f4ebea6aab06795aba
Yannik Inniger <yannik.inniger@gmail.com>
2018-11-28 17:41:10 +0000

    Add player to the model

---
+++ model/Player.py
@@ -0,0 +1,35 @@
+import time
+
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.remaining_time = 300.  # 5 Minutes in Seconds
+        self.timer = 0
+
+    def start_timer(self):
+        """
+        Starts a timer to measure the the remaining time
+        """
+        self.timer = time.perf_counter()
+
+    def stop_timer(self):
+        """
+        Stops the timer to and subtracts the elapsed time since the timer was started from the remaining time
+        """
+        end_time = time.perf_counter()
+        time_elapsed = round(end_time - self.timer)
+        self.remaining_time -= time_elapsed
+        self.timer = 0  # reset timer
+
+    def add_points(self, points_to_add):
+        """
+        Adds point to the players score
+        :param points_to_add: Positive integer to be added to the total score
+        """
+        if isinstance(points_to_add, int) and points_to_add > 0:
+            self.score += points_to_add
+        else:
+            raise ValueError('Points must be positive and of type integer')
---
+++ tests/model/PlayerTests.py
@@ -0,0 +1,38 @@
+import time
+import unittest
+
+from model.Player import Player
+
+
+class PlayerTest(unittest.TestCase):
+
+    def test_should_run_of_10_seconds(self):
+        player = Player('test')
+        player.start_timer()
+        time.sleep(10)
+        player.stop_timer()
+
+        expected_remaining_time = 290
+        remaining_time = player.remaining_time
+        self.assertEqual(expected_remaining_time, remaining_time)
+
+    def test_should_add_points(self):
+        player = Player('test')
+        player.add_points(1)
+        player.add_points(2)
+        player.add_points(3)
+
+        expected_score = 6
+        score = player.score
+        self.assertEqual(expected_score, score)
+
+    def test_should_raise_error_on_negative_points(self):
+        player = Player('test')
+        with self.assertRaises(ValueError):
+            player.add_points(-1)
+        with self.assertRaises(ValueError):
+            player.add_points(0)
+
+        expected_score = 0
+        score = player.score
+        self.assertEqual(expected_score, score)
