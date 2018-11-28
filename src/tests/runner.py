b910e7314e729d9c6189625c9b32f5dce0725b38
Yannik Inniger <yannik.inniger@gmail.com>
2018-11-28 23:42:11 +0000

    Change of Draughts test

--- src/tests/runner.py
+++ src/tests/runner.py
@@ -1,13 +1,13 @@
 import unittest
 
-from tests.control.DraughtsTest import TestDraughts
+from tests.control.DraughtsTest import DraughtsTest
 from tests.model.PlayerTest import PlayerTest
 
 
 def suite():
     test_loader = unittest.TestLoader()
     test_suite = unittest.TestSuite()
-    test_suite.addTests(test_loader.loadTestsFromTestCase(TestDraughts))
+    test_suite.addTests(test_loader.loadTestsFromTestCase(DraughtsTest))
     test_suite.addTests(test_loader.loadTestsFromTestCase(PlayerTest))
     return test_suite
 
