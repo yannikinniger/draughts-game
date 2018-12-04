a2a60d89ce25323dbad310f237a9b5ff7d4daa63
Yannik Inniger <yannik.inniger@gmail.com>
2018-12-04 21:15:39 +0000

    Add GuiMixin

---
+++ src/view/GuiMixin.py
@@ -0,0 +1,16 @@
+
+class GuiMixin:
+
+    def __init__(self):
+        self.__init_components()
+        self.__layout()
+        self.__setup_bindings()
+
+    def __init_components(self):
+        pass
+
+    def __layout(self):
+        pass
+
+    def __setup_bindings(self):
+        pass
