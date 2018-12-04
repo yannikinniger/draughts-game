4434eb0656825635da4ffff8a711ecf64719e6ab
Yannik Inniger <yannik.inniger@gmail.com>
2018-12-04 20:14:52 +0000

    Create concrete game object

---
+++ src/control/DraughtsGame.py
@@ -0,0 +1,19 @@
+from control.AbstractDraughts import AbstractDraughts
+
+
+class DraughtsGame(AbstractDraughts):
+
+    def __init__(self, player_1, player_2):
+        super().__init__(player_1, player_2)
+
+    def pause(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def click_event(self, row, column):
+        pass
+
+    def key_event(self, key_event):
+        pass
