aff9faeae640ba7c9374d4c96f1f5e244d3fe7c8
Yannik Inniger <yannik.inniger@gmail.com>
2018-12-10 16:50:52 +0000

    Add temporary implementation of possible_moves

--- src/control/DraughtsGame.py
+++ src/control/DraughtsGame.py
@@ -19,4 +19,10 @@ class DraughtsGame(AbstractDraughts):
         pass
 
     def get_possible_moves(self, selected_row, selected_column):
-        pass
+        # Todo override this code, it's just for temporary testing
+        board = []
+        for row in range(0, self.board_size):
+            board.append([0] * self.board_size)
+
+        board[selected_row + 1][selected_column + 1] = 1
+        return board
