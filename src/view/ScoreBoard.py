679915d9695483f95103bfbc5ef8e79002cf550d
Yannik Inniger <yannik.inniger@gmail.com>
2018-12-15 15:38:07 +0000

    Change font on scoreboard

--- src/view/ScoreBoard.py
+++ src/view/ScoreBoard.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDockWidget, QLabel, QWidget, QGridLayout, QSpacerItem, QSizePolicy
+from qtpy import QtGui
 
 from helper.Observer import Observer
 from view.GuiMixin import GuiMixin
@@ -13,15 +14,16 @@ class ScoreBoard(QDockWidget, GuiMixin, Observer):
         GuiMixin.__init__(self)
 
     def _init_components(self):
-        self.player_1_label = ScoreBoard.get_label(self.draughts_game.player_1.name)
-        self.player_1_score = ScoreBoard.get_label("Score: {}".format(self.draughts_game.player_1.score))
-        self.player_2_label = ScoreBoard.get_label(self.draughts_game.player_2.name)
-        self.player_2_score = ScoreBoard.get_label("Score: {}".format(self.draughts_game.player_2.score))
+        self.player_1_label = ScoreBoard.get_label(self.draughts_game.player_1.name, 16)
+        self.player_1_score = ScoreBoard.get_label("Score: {}".format(self.draughts_game.player_1.score), 14)
+        self.player_2_label = ScoreBoard.get_label(self.draughts_game.player_2.name, 16)
+        self.player_2_score = ScoreBoard.get_label("Score: {}".format(self.draughts_game.player_2.score), 14)
 
     @staticmethod
-    def get_label(content):
+    def get_label(content, font_size):
         label = QLabel(content)
         label.setAlignment(Qt.AlignCenter)
+        label.setFont(QtGui.QFont('SansSerif', font_size))
         return label
 
     def _layout(self):
